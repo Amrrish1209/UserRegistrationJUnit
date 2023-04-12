ddc3edabf61829d1b425acf2b9eb472dea29e886
Amrrish1209 <roshanbabubl@gmail.com>
2023-04-12 08:33:10 +0530

    Add:As a User need to enter a valid Last Name

--- src/main/java/com/bridgelabz/userregistration/UserRegistrations.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistrations.java
@@ -11,13 +11,13 @@ public class UserRegistrations {
 		// ^->beginning of the string,A-Z->first character should be upper
 		// case,a-zA-Z->any alphabetical character,{2,}->max of 3 character,$->end of
 		// the string
-		String firstNameRegex = "^[A-Z][a-zA-Z]{2,}$";
-		System.out.println("Enter the FirstName: ");
+		String lastNameRegex = "^[A-Z][a-zA-Z]{2,}$";
+		System.out.println("Enter the LastName: ");
 		Scanner sc = new Scanner(System.in);
-		String firstName = sc.nextLine();
+		String lastName = sc.nextLine();
 
-		Pattern pattern = Pattern.compile(firstNameRegex);
-		Matcher matcher = pattern.matcher(firstName);
+		Pattern pattern = Pattern.compile(lastNameRegex);
+		Matcher matcher = pattern.matcher(lastName);
 		boolean result = matcher.matches();
 
 		if (result) {
