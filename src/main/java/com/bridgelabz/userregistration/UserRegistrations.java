fc141c53b9b27765a9ce36b05db18dd74dfeebff
Amrrish1209 <roshanbabubl@gmail.com>
2023-04-12 08:36:31 +0530

    Add:As a User need to follow pre -defined Mobile Format

--- src/main/java/com/bridgelabz/userregistration/UserRegistrations.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistrations.java
@@ -8,19 +8,22 @@ public class UserRegistrations {
 
 	public static void main(String[] args) {
 
-		String emailRegex = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\\.[a-zA-Z]{2,}){1,2}$";
-		System.out.println("Enter the email: ");
-		Scanner sc = new Scanner(System.in);
-		String email = sc.nextLine();
+		// ^->beginning of string,[0-9]{2}->exactly two numeric
+		// number,[0-9][10]->exactly ten numeric numbers,$->end of string
+		String mobileRegex = "^[0-9]{2} [0-9]{10}$";
 
-		Pattern pattern = Pattern.compile(emailRegex);
-		Matcher matcher = pattern.matcher(email);
+		System.out.println("Enter the Mobile number: ");
+		Scanner sc = new Scanner(System.in);
+		String mobileNumber = sc.nextLine();
+
+		Pattern pattern = Pattern.compile(mobileRegex);
+		Matcher matcher = pattern.matcher(mobileNumber);
 		boolean result = matcher.matches();
 
 		if (result) {
-			System.out.println("Pattern Match");
+			System.out.println("Valid mobile number");
 		} else {
-			System.out.println("Pattern not Match");
+			System.out.println("Invalid mobile number");
 		}
 		sc.close();
 	}
